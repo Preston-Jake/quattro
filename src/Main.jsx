041b70cf781b71a1e9152ec203899ee4c6154e48
JakePreston <prestonjak@outlook.com>
2020-08-04 08:08:51 -0500

    1 day build

---
+++ src/Main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Main.css";
+
+export default function Main() {
+  return (
+    <main>
+      <div className="legend"></div>
+      <div className="machine">
+        <h2>The Machine</h2>
+      </div>
+      <div className="events">
+        <h2>Events / Stages</h2>
+      </div>
+      <div className="driver">
+        <h2>The Team</h2>
+      </div>
+    </main>
+  );
+}
---
+++ src/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Nav() {
+    return (
+        <nav>
+            <img src="" alt=""/>
+            <h2>Audi Team</h2>
+            <ul>
+                <li>Driver</li>                
+                <li>Events</li>
+                <li>Machine</li>
+                <li>About</li>
+            </ul>
+        </nav>
+    )
+}
